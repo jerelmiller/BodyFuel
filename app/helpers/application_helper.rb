b19fb97d9a1841422941d37dff38039a8be35a5c
Jerel Miller <jermille@mines.edu>
2013-06-22 02:03:00 -0600

    Fixing footer being too far down page without content on it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def facebook_link
-    link_to 'https://www.facebook.com/bodyfuel.colorado', target: :_blank do
-      content_tag :span, '', class: 'icon-facebook facebook'
+    link_to 'https://www.facebook.com/bodyfuel.colorado', target: :_blank, class: 'facebook' do
+      content_tag :span, '', class: 'icon-facebook'
     end
   end
 end
