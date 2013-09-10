01afb703308d3b0e410ad9e731e4df1941e918a2
Jerel Miller <jermille@mines.edu>
2013-09-10 01:19:03 -0600

    Fixing image and link in mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,13 @@
 class ContactMailer < ActionMailer::Base
   layout 'mailer'
-
+  helper ActionView::Helpers::UrlHelper
 
   def send_contact_email(name, email, message)
     @name = name
     @email = email
     @message = message
+
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/logo.png")
 
     mail to: 'jerelmiller@gmail.com', subject: "New message from #{name}", from: 'jerelmiller@gmail.com', reply_to: 'jerelmiller@gmail.com'
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ BodyFuel::Application.configure do
     :authentication       => 'plain',
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = {
+    :host => 'bodyfuel.heroku.com'
+  }
 end
