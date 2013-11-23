d9ccfb68fb9771834dc46791f30378d99e0b549c
Jerel Miller <jerelmiller@gmail.com>
2013-11-22 20:51:00 -0700

    Adding way of defining which stripe account to use

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,13 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
-}
+if ENV['USE_STRIPE_LIVE']
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['LIVE_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['LIVE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['TEST_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['TEST_SECRET_KEY']
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
